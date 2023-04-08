816005c23af47014b6a833d59559bd2f0cd840e7
Alex Inglis <alexinglis7799@gmail.com>
2023-04-08 13:28:25 +0800

    add style to buttons and cards and create remove button and read toggle

--- script.js
+++ script.js
@@ -1,43 +1,66 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-const gbg = new Book("The Glass Bead Game", "Herman Hesse", "558", true)
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+
+function toggleRead(index) {
+  myLibrary[index].toggleRead();
+  makeList();
+}
+
+const gbg = new Book("The Glass Bead Game", "Herman Hesse", "558", true);
 myLibrary.push(gbg);
 
-
+makeList();
 
 function addBookToLibrary() {
-    let title = document.querySelector("#title").value;
-    let author = document.querySelector("#author").value;
-    let pages = document.querySelector("#pages").value;
-    let read = document.querySelector("#read").checked;
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-    console.log(myLibrary)
-    makeList();
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").checked;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+  makeList();
 }
 
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
+  document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-    addBookToLibrary();
+  document.getElementById("myForm").style.display = "none";
+  addBookToLibrary();
+}
+
+function delBook(index) {
+  myLibrary.splice(index, 1);
+  makeList();
+  console.log(d);
 }
 
 function makeList() {
-    let shelfBook = document.querySelector("#shelf")
-    for (let i = 0; i < myLibrary.length; i++) {
-        let book = myLibrary[i]
-        let spine = document.createElement("div")
-        spine.innerHTML = `<p>${book.title}<p><p>${book.author}<p><p>${book.pages}<p><p>${book.read}<p>`
-        shelfBook.appendChild(spine);
-    }
+  let shelfBook = document.querySelector("#shelf");
+  shelfBook.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let spine = document.createElement("div");
+    spine.innerHTML = `<p>${book.title}</p>
+            <p>${book.author}</p>
+            <p>${book.pages + " pages"}</p>
+            <button onclick="toggleRead(${i})">${
+      book.read ? "Read" : "Not Read Yet"
+    }</p>
+            <button class="remove-btn" onclick="delBook(${i})">Remove</button>`;
+    spine.className = "spine-div";
+    shelfBook.appendChild(spine);
+  }
 }
